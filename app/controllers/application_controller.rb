23903180e2f7d9564494a8834fd86e76d2e6953a
James Maynard <jameskirkmaynard@gmail.com>
2016-11-20 07:38:33 -0800

    add responders gem

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  repond_to :json
+  respond_to :json
   def angular
     render 'layouts/application'
   end
