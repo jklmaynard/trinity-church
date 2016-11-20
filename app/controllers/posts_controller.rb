41b91709f8f82ff71cb7a51a8e31eaff3f8f9666
James Maynard <jameskirkmaynard@gmail.com>
2016-11-20 07:27:14 -0800

    Configuring routes for posts and post class

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,20 @@
+class PostsController < ApplicationController
+
+
+  def index
+    respond_with Post.all
+  end
+
+  def create
+    respond_with Post.create(post_params)
+  end
+
+  def show
+    respond_with Post.find(params[:id])
+  end
+
+  private
+  def post_params
+    params.require(:post).permit(:link, :title)
+  end
+end
