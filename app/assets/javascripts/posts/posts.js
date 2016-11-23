29592953b6c39148b8d1192a5db17b5290bd47c7
James Maynard <jameskirkmaynard@gmail.com>
2016-11-23 14:36:03 -0800

    build out skeleton of posts-home

--- app/assets/javascripts/posts/posts.js
+++ app/assets/javascripts/posts/posts.js
@@ -1,8 +1,20 @@
 angular.module('trinityChurch')
   .factory('posts', [function(){
       var obj = {
-        posts: [],
+        posts: [
+          {
+            title: "This is my first message",
+            body: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+            is_event: false
+          },
+          {
+            title: "This is my SECOND message",
+            body: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+            is_event: false
+          }
+        ],
         events: []
       };
       return obj
 }])
+""
