981ea48c79614c58de92a375841af12813cf7f23
James Maynard <jameskirkmaynard@gmail.com>
2017-02-14 11:38:17 -0800

    Add '$' to scope, dummy

--- app/assets/javascripts/nav/navCtrl.js
+++ app/assets/javascripts/nav/navCtrl.js
@@ -8,14 +8,11 @@ angular.module('trinityChurch')
     Auth.currentUser().then(function (user){
       $scope.user = user;
     });
-    $scope.$on('devise:new-registration', function(e, user) {
-      $scope.user = user;
-    });
     $scope.$on('devise:login', function(e, user) {
       $scope.user = user;
     });
     $scope.$on('devise:logout', function(e, user) {
-      scope.user = {};
-    })
+      $scope.user = {};
+    });
   }
 ]);
